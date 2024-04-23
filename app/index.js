de8e68cd0521da871f46f92f366258900b23319d
pranavrai070 <pranavrai070@gmail.com>
2024-04-23 12:34:54 +0530

    tailwind config

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-300">
+      <Text className="text-3xl">Open up App.js start app!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{color:"blue"}}>Go to Profile</Link>
+    </View>
+  );
+}
+
